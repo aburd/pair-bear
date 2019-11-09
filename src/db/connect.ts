77d389340b04a2071ac30a6d2d9836c2e259a8e4
aburd <burdick.aaron@gmail.com>
2019-11-10 03:33:58 +0900

    add connection to db functionality

---
+++ src/db/connect.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_DOMAIN,
+  DB_NAME,
+} = process.env;
+
+async function connect(): Promise<mongoose.Connection> {
+  const dbUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_DOMAIN}/${DB_NAME}`
+  console.log('Connecting to DB...')
+  await mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'));
+  console.log('DB sucessfully connected!')
+  return db
+}
+
+export default connect
