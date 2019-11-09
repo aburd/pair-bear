f9731c59a25e354e25ef45df1f697e0c2d361268
aburd <burdick.aaron@gmail.com>
2019-11-10 03:11:26 +0900

    add some basic events

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,21 @@ const app = new App({
 app.message('hello', ({ message, say }) => {
   say({
     blocks: [
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `Hey there <@${message.user}>!`
-      },
-      "accessory": {
-        "type": "button",
+      {
+        "type": "section",
         "text": {
-          "type": "plain_text",
-          "text": "Click Me"
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
         },
-        "action_id": "button_click"
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
       }
-     }
     ]
   });
 });
@@ -34,6 +34,34 @@ app.action('button_click', ({ body, ack, say }) => {
 
   say(`<@${body.user.id}> clicked the button.`)
 });
+
+
+
+// Sends a section block with datepicker when someone reacts with a 📅 emoji
+app.event('reaction_added', ({ event, say }) => {
+  if (event.reaction === 'calendar') {
+    say({
+      blocks: [{
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Pick a date for me to remind you"
+        },
+        "accessory": {
+          "type": "datepicker",
+          "action_id": "datepicker_remind",
+          "initial_date": "2019-04-28",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select a date"
+          }
+        }
+      }]
+    });
+  }
+});
+
+
 
 (async () => {
   await app.start(process.env.PORT || 3000);
