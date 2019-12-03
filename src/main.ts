c0e432be7e6309d33791b5cde295aef8f8e2a5c9
aburd <burdick.aaron@gmail.com>
2019-12-03 15:20:08 +0900

    change up invite modal

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,16 @@ import {
   showReceived,
   showSent,
 } from './processes/invites'
+import { createInviteModal } from './processes/inviteModal'
 import { User, Invite } from './db/models'
 import { Confirmation } from './db/models/Invite'
-import { Actions } from './typings'
+import { Actions, Views } from './typings'
 
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  endpoints: { events: '/slack/events', commands: '/slack/commands' }
+  endpoints: { events: '/slack/events', commands: '/slack/commands', actions: '/slack/actions' }
 });
 
 // middleware
@@ -33,6 +34,11 @@ app.message(/help|who are you\??/i, async (args) => {
   await handleUsers(args)
   await help(args)
 })
+
+// hello
+app.message(/^(hello|hi|こんにちは|こんばんは|hey)$/i, async (args) => {
+  await greet(args)
+});
 
 // invites
 app.command(`/invites`, async (args) => {
@@ -83,46 +89,31 @@ app.action(Actions.inviteDeny, async ({ ack, payload, say }) => {
   say({ blocks: await invite.toBlocks() })
 })
 
-app.action(Actions.inviteCreateInvite, ({ ack, payload, context }) => {
+app.action(Actions.inviteCreateInvite, async ({ ack, payload, body, context }) => {
   // Acknowledge the command request
   ack();
-  console.log('payload.trigger_id', payload)
-  console.log('context', context)
+  const inviteModal = await createInviteModal()
+  console.log(JSON.stringify(inviteModal, null, 2))
   try {
     const result = app.client.views.open({
       token: context.botToken,
       // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: 'invite_create_modal',
+      trigger_id: body.trigger_id,
       // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
-        title: {
-          type: 'plain_text',
-          text: 'Modal title'
-        },
-        blocks: [],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
-        }
-      }
+      view: inviteModal,
     });
-    console.log(result);
+    console.log(result)
   }
   catch (error) {
-    console.error(error);
+    console.error(error)
   }
 });
 
-
-
-// hello
-app.message(/^(hello|hi|こんにちは|こんばんは|hey)$/i, async (args) => {
-  await greet(args)
+app.view(Views.inviteCreated, async args => {
+  args.ack();
+  await handleUsers(args)  
+  console.log('args from invite_created', args)
 });
-
 
 (async () => {
   const db = await connect()
--- src/processes/inviteModal.ts
+++ src/processes/inviteModal.ts
@@ -1,108 +1,133 @@
-export default {
-  "type": "modal",
-  "title": {
-    "type": "plain_text",
-    "text": "Create an Invitation",
-    "emoji": true
-  },
-  "submit": {
-    "type": "plain_text",
-    "text": "Create",
-    "emoji": true
-  },
-  "close": {
-    "type": "plain_text",
-    "text": "Cancel",
-    "emoji": true
-  },
-  "blocks": [
-    {
-      "type": "input",
-      "element": {
-        "type": "multi_channels_select",
-        "action_id": "theme",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "What is the theme of the pair-programming?"
-        }
-      },
-      "label": {
-        "type": "plain_text",
-        "text": "Theme"
-      }
+import { Invite } from '../db/models'
+import { Views } from '../typings'
+
+export async function createInviteModal() {
+  const options = await userOptions()
+  return {
+    "type": "modal",
+    "callback_id": Views.inviteCreated,
+    "title": {
+      "type": "plain_text",
+      "text": "Create an Invitation",
+      "emoji": true
     },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Pick an engineer to pair-program with"
+    "submit": {
+      "type": "plain_text",
+      "text": "Create",
+      "emoji": true
+    },
+    "close": {
+      "type": "plain_text",
+      "text": "Cancel",
+      "emoji": true
+    },
+    "blocks": options.length
+      ? [
+        themeBlock(),
+        await engineerBlock(options),
+        datepickerBlock(),
+        timeBlock(),
+      ]
+      : [sorryBlock()]
+  }
+}
+
+async function engineerBlock(engineerOptions) {
+  return {
+    "type": "input",
+    "element": {
+      "type": "static_select",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "Takakuda",
+        emoji: true,
       },
-      "accessory": {
-        "type": "static_select",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "Takakuda",
-          "emoji": true
+      "options": engineerOptions
+  },
+    "label": {
+      "type": "plain_text",
+      "text": "Takakuda",
+      "emoji": true
+    }
+  }
+}
+
+async function userOptions() {
+  return await Invite.notInvitedUsers()
+    .then(users => users.map(u => u.toSlackOption()))
+    .catch(e => {
+      console.error(e)
+      return [{
+        text: {
+          type: 'plain_text',
+          text: e.message
         },
-        "options": [
-          {
-            "text": {
-              "type": "plain_text",
-              "text": "Choice 1",
-              "emoji": true
-            },
-            "value": "value-0"
-          },
-          {
-            "text": {
-              "type": "plain_text",
-              "text": "Choice 2",
-              "emoji": true
-            },
-            "value": "value-1"
-          },
-          {
-            "text": {
-              "type": "plain_text",
-              "text": "Choice 3",
-              "emoji": true
-            },
-            "value": "value-2"
-          }
-        ]
-      }
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Pick a day to pair-program"
-      },
-      "accessory": {
-        "type": "datepicker",
-        "initial_date": "1990-04-28",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "Select a date",
-          "emoji": true
-        }
-      }
-    },
-    {
-      "type": "input",
-      "element": {
-        "type": "multi_channels_select",
-        "action_id": "theme",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "hh:mm"
-        }
-      },
-      "label": {
+        value: 'error'
+      }]
+    })
+}
+
+function themeBlock() {
+  return {
+    "type": "input",
+    "element": {
+      "type": "plain_text_input",
+      "action_id": "theme",
+      "placeholder": {
         "type": "plain_text",
-        "text": "What time do you want to pair program?",
+        "text": "What is the theme of the pair-programming?"
+      }
+    },
+    "label": {
+      "type": "plain_text",
+      "text": "Theme"
+    }
+  }
+}
+
+function datepickerBlock() {
+  return {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Pick a day to pair-program"
+    },
+    "accessory": {
+      "type": "datepicker",
+      "initial_date": "1990-04-28",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "Select a date",
         "emoji": true
       }
     }
-  ]
+  }
+}
+
+function timeBlock() {
+  return {
+    "type": "input",
+    "element": {
+      "type": "plain_text_input",
+      "action_id": "time",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "hh:mm"
+      }
+    },
+    "label": {
+      "type": "plain_text",
+      "text": "What time do you want to pair program?",
+    }
+  }
+}
+
+function sorryBlock() {
+  return {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Sorry, there aren't any engineers who are ready to program."
+    }
+  }
 }
