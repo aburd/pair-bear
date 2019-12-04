131ebcc0c370607625ece60bf6428102612b36ad
Aaron Burdick <trashctrl@Aarons-MacBook-Air.local>
2019-12-05 02:28:50 +0900

    Improve reminder

--- src/cron/invites.ts
+++ src/cron/invites.ts
@@ -5,9 +5,8 @@ import { formatDate } from '../lib/helpers'
 const token = process.env.SLACK_BOT_TOKEN
 
 async function remindConfirmed (app: bolt.App) {
-  console.log('Reminding users...')
   const minDate = new Date()
-  const maxDate = new Date(minDate.valueOf() + (1000 * 60))
+  const maxDate = new Date(minDate.valueOf() + (1000 * 60 - 1))
   const invites = await Invite.findConfirmedByRange(minDate, maxDate)
   invites.forEach(async invite => sendReminder(app, invite))
 }
@@ -19,12 +18,20 @@ async function sendReminder(app, invite) {
     token,
     channel: to.channel,
     text: `Reminder: You have pair programming today at ${formatDate(invite.date)}`,
+  })
+  app.client.chat.postMessage({
+    token,
+    channel: to.channel,
     blocks: await invite.toBlocks(to.userId)
   })
   app.client.chat.postMessage({
     token,
     channel: from.channel,
     text: `Reminder: You have pair programming today at ${formatDate(invite.date)}`,
+  })
+  app.client.chat.postMessage({
+    token,
+    channel: from.channel,
     blocks: await invite.toBlocks(from.userId)
   })
 }
